251eb34fc5ee0e35bca9fb573d5d1d326fb69bce
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-02-25 22:24:07 -0500

    Created RandomGenerator function

--- project.js
+++ project.js
@@ -3,3 +3,10 @@ let listOfDestinations = ['Japan', 'Korea', 'Italy', 'California', 'Mexico', 'Al
 let listOfRestaurants = ['Borders Cafe', 'Maharaja', 'Sakura', 'Burger King', 'Taco Bell', 'Sonic', 'Five Guys', 'Chipotle'];
 let transportationList = ['car', 'plane', 'jet', 'limo', 'motorcycle', 'subway', 'bus', 'train'];
 let entertainmentList = ['Movies', 'art show', 'snowboarding', 'skatepark', 'thrifting', 'concert', 'festival', 'wine and beer tasting'];
+
+function RandomGenerator(array){
+    let randomChoice = array[Math.floor(Math.random()* array.length)]
+    return randomChoice;
+};
+
+//console.log(RandomGenerator(listOfDestinations));
