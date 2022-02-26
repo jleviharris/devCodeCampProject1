b231435c89cd32f6ab4bb28ea5fc95f69c6f53ad
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-02-25 21:53:47 -0500

    Created variables for the distinations, restaurants, transportation, and entertainment

--- project.js
+++ project.js
@@ -1,1 +1,5 @@
 "use strict"
+let listOfDestinations = ['Japan', 'Korea', 'Italy', 'California', 'Mexico', 'Alaska', 'Germany'];
+let listOfRestaurants = ['Borders Cafe', 'Maharaja', 'Sakura', 'Burger King', 'Taco Bell', 'Sonic', 'Five Guys', 'Chipotle'];
+let transportationList = ['car', 'plane', 'jet', 'limo', 'motorcycle', 'subway', 'bus', 'train'];
+let entertainmentList = ['Movies', 'art show', 'snowboarding', 'skatepark', 'thrifting', 'concert', 'festival', 'wine and beer tasting'];
