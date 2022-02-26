576404720f175da6057a39d70519d8d837b83295
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-02-25 22:44:23 -0500

    Created DestinationsGenerator function

--- project.js
+++ project.js
@@ -8,5 +8,23 @@ function RandomGenerator(array){
     let randomChoice = array[Math.floor(Math.random()* array.length)]
     return randomChoice;
 };
-
+//console.log check bellow
 //console.log(RandomGenerator(listOfDestinations));
+
+function DestinationsGenerator (){
+    let userIsHappy = false;
+    
+    while (userIsHappy === false){
+        let randomDest = RandomGenerator(listOfDestinations)
+        let userResponse = prompt(`Do you want to go to ${randomDest}?`)
+
+        if (userResponse.toLowerCase()==='yes'){
+            alert(`Great Choice. Have fun in ${randomDest}!`)
+            userIsHappy = true
+        }else if (userResponse.toLowerCase()==='no'){
+            userIsHappy = false
+        }else alert('Please choose "yes" or "no"')
+        }
+    };
+
+DestinationsGenerator();
